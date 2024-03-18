54ffcec17cf8cdacfc0d6c0a5687a38026f2c41f
Maya Azar <mayaazar@Mayas-Air.cgocable.net>
2024-03-18 14:27:49 -0400

    created seperate class to manage current position and creeks (close #30)

--- src/main/java/ca/mcmaster/se2aa4/island/team113/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team113/Drone.java
@@ -18,26 +18,16 @@ public class Drone {
     ScanIsland islandScanner;
     private boolean initialScanner;
     private Direction initialDirection;
-    private Direction mapDirection;
-    private MapOfPoints map;
-    private Coordinate position;
-    private ArrayList<String> allCreeks;
-    private double distance;
-    private double newDistance;
-    private String closestCreek;
+    private PositionTrack mapTrack;
 
 
     public Drone(Integer battery, String direction){
         this.initialDirection = Direction.stringToDirection(direction);
         this.battery = new DroneBattery(battery);
         this.initialScanner= false;
-        this.mapDirection= initialDirection;
-        this.map = new MapOfPoints();
-        this.position = new Coordinate(0,0);
         ground = new GoToGround(initialDirection);
-        this.allCreeks = new ArrayList<>();
-        this.distance = Double.POSITIVE_INFINITY;
-        this.closestCreek = "";
+        this.mapTrack = new PositionTrack(initialDirection);
+
         
         
     }
@@ -48,68 +38,6 @@ public class Drone {
             initialScanner = true;
 
         }
-    }
-    private void positionTracker(JSONObject decision){
-        if(decision.similar(command.TurnLeft(mapDirection))){
-            logger.info("GOING LEFT");
-            logger.info("{}",command.TurnLeft(mapDirection).toString(2));
-            position = position.turnLeft(mapDirection);
-            mapDirection = mapDirection.goLeft();
-
-        } else if (decision.similar(command.TurnRight(mapDirection))){
-            logger.info("GOING RIGHT");
-            logger.info("{}",command.TurnRight(mapDirection).toString(2));
-            position = position.turnRight(mapDirection);
-            mapDirection = mapDirection.goRight();
-
-        }else if (decision.similar(command.fly())){
-            logger.info("GOING FORWARD");
-            logger.info("MAP DIRECTION {}", mapDirection.directionToString());
-            logger.info("{}",command.fly().toString(2));
-            position = position.forward(mapDirection);
-        }
-        logger.info(" X COORDINATE {}", position.getX());
-        logger.info(" Y COORDINATE {}", position.getY());
-
-    }
-
-    private void mapUpdate(Information info){
-        JSONObject extras = info.getExtras();
-        if (extras.has("creeks")){
-            JSONArray creeks = extras.getJSONArray("creeks");
-            if (creeks.length() > 0){
-                for (int i=0; i<creeks.length(); i++){
-                    map.addPointOfInterest(creeks.optString(i), position.getX(), position.getY());
-                    allCreeks.add(creeks.optString(i));
-                }
-            }
-        }
-        if (extras.has("sites")){
-            JSONArray sites = extras.getJSONArray("sites");
-            if (sites.length() > 0){
-                for (int i=0; i<sites.length(); i++){
-                    map.addPointOfInterest("site", position.getX(), position.getY());
-                }
-            }
-        }
-
-    }
-    private String findClosestCreek(){
-        logger.info("SITE X COORDINATE {}", map.getPointOfInterest("site").getX());
-        logger.info("SITE Y COORDINATE {}", map.getPointOfInterest("site").getY());
-        for (int i =0; i< allCreeks.size(); i++){
-            newDistance = map.calculateDistance("site", allCreeks.get(i));
-            logger.info("CREEK ID: {}", allCreeks.get(i));
-            logger.info("DISTENCE FROM SITE {}", newDistance);
-            logger.info("CREEK X COORDINATE {}", map.getPointOfInterest(allCreeks.get(i)).getX());
-            logger.info("CREEK Y COORDINATE {}", map.getPointOfInterest(allCreeks.get(i)).getY());
-            if (newDistance < distance){
-                distance = newDistance;
-                closestCreek = allCreeks.get(i);
-            }
-        }
-        return closestCreek;
-        
     }
 
     public void resultCheck(Information info){ 
@@ -123,7 +51,7 @@ public class Drone {
             logger.info("CURRENT DIRECTION 1 {}", currentDirection.directionToString());
             islandScanner.resultCheck(info);
         }
-        mapUpdate(info);
+        mapTrack.mapUpdate(info);
 
 
         
@@ -143,10 +71,10 @@ public class Drone {
         }
         logger.info("BEGIN MAP");
         logger.info("{}",decision.toString(2));
-        positionTracker(decision);
+        mapTrack.positionTracker(decision);
 
         if(decision.getString("action").equals("stop")){
-            logger.info("THE CLOSEST CREEK IS {}", findClosestCreek());
+            logger.info("THE CLOSEST CREEK IS {}", mapTrack.findClosestCreek());
         }
        
         return decision;
---
+++ src/main/java/ca/mcmaster/se2aa4/island/team113/PositionTrack.java
@@ -0,0 +1,100 @@
+package ca.mcmaster.se2aa4.island.team113;
+
+import java.util.ArrayList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class PositionTrack {
+    private final Logger logger = LogManager.getLogger();
+    private MapOfPoints map;
+    private Direction currentDirection;
+    private Coordinate position;
+    private ArrayList<String> allCreeks;
+    private double distance;
+    private double newDistance;
+    private String closestCreek;
+    Commands command = new Commands();
+
+    public PositionTrack(Direction initialDirection){
+        this.map = new MapOfPoints();
+        this.currentDirection=initialDirection;
+        this.allCreeks = new ArrayList<>();
+        this.distance = Double.POSITIVE_INFINITY;
+        this.closestCreek = "";
+        this.position = new Coordinate(0,0);
+
+        
+    }
+
+
+    public String findClosestCreek(){
+        logger.info("SITE X COORDINATE {}", map.getPointOfInterest("site").getX());
+        logger.info("SITE Y COORDINATE {}", map.getPointOfInterest("site").getY());
+        for (int i =0; i< allCreeks.size(); i++){
+            newDistance = map.calculateDistance("site", allCreeks.get(i));
+            logger.info("CREEK ID: {}", allCreeks.get(i));
+            logger.info("DISTENCE FROM SITE {}", newDistance);
+            logger.info("CREEK X COORDINATE {}", map.getPointOfInterest(allCreeks.get(i)).getX());
+            logger.info("CREEK Y COORDINATE {}", map.getPointOfInterest(allCreeks.get(i)).getY());
+            if (newDistance < distance){
+                distance = newDistance;
+                closestCreek = allCreeks.get(i);
+            }
+        }
+        return closestCreek;
+        
+    }
+
+    public void positionTracker(JSONObject decision){
+        if(decision.similar(command.TurnLeft(currentDirection))){
+            logger.info("GOING LEFT");
+            logger.info("{}",command.TurnLeft(currentDirection).toString(2));
+            position = position.turnLeft(currentDirection);
+            currentDirection = currentDirection.goLeft();
+
+        } else if (decision.similar(command.TurnRight(currentDirection))){
+            logger.info("GOING RIGHT");
+            logger.info("{}",command.TurnRight(currentDirection).toString(2));
+            position = position.turnRight(currentDirection);
+            currentDirection = currentDirection.goRight();
+
+        }else if (decision.similar(command.fly())){
+            logger.info("GOING FORWARD");
+            logger.info("MAP DIRECTION {}", currentDirection.directionToString());
+            logger.info("{}",command.fly().toString(2));
+            position = position.forward(currentDirection);
+        }
+        logger.info(" X COORDINATE {}", position.getX());
+        logger.info(" Y COORDINATE {}", position.getY());
+
+    }
+
+    public void mapUpdate(Information info){
+        JSONObject extras = info.getExtras();
+        if (extras.has("creeks")){
+            JSONArray creeks = extras.getJSONArray("creeks");
+            if (creeks.length() > 0){
+                for (int i=0; i<creeks.length(); i++){
+                    map.addPointOfInterest(creeks.optString(i), position.getX(), position.getY());
+                    allCreeks.add(creeks.optString(i));
+                }
+            }
+        }
+        if (extras.has("sites")){
+            JSONArray sites = extras.getJSONArray("sites");
+            if (sites.length() > 0){
+                for (int i=0; i<sites.length(); i++){
+                    map.addPointOfInterest("site", position.getX(), position.getY());
+                }
+            }
+        }
+
+    }
+
+
+
+    
+}
