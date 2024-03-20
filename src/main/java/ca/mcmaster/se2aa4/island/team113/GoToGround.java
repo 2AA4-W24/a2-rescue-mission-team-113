563103ed0d1eb53849a4697cbbefb142f356a7fb
Amirah Abdel-Rahman <amirah@Amirahs-MacBook-Pro.local>
2024-03-20 18:16:53 -0400

    moved GoToGround states to be sublasses of GoToGround (close #36)

--- src/main/java/ca/mcmaster/se2aa4/island/team113/GoToGround.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team113/GoToGround.java
@@ -1,5 +1,8 @@
 package ca.mcmaster.se2aa4.island.team113;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class GoToGround implements DecisionMaker{
@@ -72,4 +75,199 @@ public class GoToGround implements DecisionMaker{
         this.currentDirection= map.getcurrentDirection();
         return decision;
     }
+
+    private interface GoToGroundState {
+        JSONObject handleNextState(GoToGround context);
+    }
+    
+
+    private class Fly implements GoToGroundState{
+
+        @Override
+        public JSONObject handleNextState(GoToGround context) {
+            JSONObject decision = new JSONObject();
+            Commands command = new Commands();
+            
+            decision = command.echo(context.getCurrentDirection());
+            context.setEchoDirection(context.getCurrentDirection());
+            context.setState(new EchoFront());
+    
+            return decision;
+        }
+        
+    }
+
+    private class EchoFront implements GoToGroundState{
+
+        @Override
+        public JSONObject handleNextState(GoToGround context) {
+            JSONObject decision = new JSONObject();
+            Commands command = new Commands();
+            Information info = context.getInfo();
+            JSONObject extras = info.getExtras();
+    
+            if (extras.getString("found").equals("OUT_OF_RANGE")){
+                decision = command.echo(context.getCurrentDirection().goRight());
+                context.setEchoDirection(context.getCurrentDirection().goRight());
+    
+                context.setState(new EchoRight());
+    
+            }else{
+                decision = command.echo(context.getCurrentDirection());
+                context.setState(new GroundRange());
+            }
+    
+            return decision;
+    
+            
+        }
+        
+    }
+
+    private class EchoRight implements GoToGroundState{
+
+        @Override
+        public JSONObject handleNextState(GoToGround context) {
+            JSONObject decision = new JSONObject();
+            Commands command = new Commands();
+            Information info = context.getInfo();
+            JSONObject extras = info.getExtras();
+    
+            if (extras.getString("found").equals("OUT_OF_RANGE")){
+                decision = command.echo(context.getCurrentDirection().goLeft());
+                context.setEchoDirection(context.getCurrentDirection().goLeft());
+    
+                context.setState(new EchoLeft());
+    
+            }else{
+                context.setState(new TurnToGround());
+                Direction turndirection = context.getCurrentDirection();
+                decision = command.Turn(turndirection.goRight());
+            }
+            return decision;
+        }
+        
+    }
+    
+    private class EchoLeft implements GoToGroundState {
+        @Override
+        public JSONObject handleNextState(GoToGround context) {
+            JSONObject decision = new JSONObject();
+            Commands command = new Commands();
+            Information info = context.getInfo();
+            JSONObject extras = info.getExtras();
+    
+            if (extras.getString("found").equals("OUT_OF_RANGE")){
+                decision = command.fly();
+    
+                context.setState(new Fly());
+    
+            }else{
+                context.setState(new TurnToGround());
+                decision = command.Turn(context.getCurrentDirection().goLeft());
+            }
+            return decision;
+        }
+        
+    }
+
+    private class GroundRange implements GoToGroundState{
+
+        @Override
+        public JSONObject handleNextState(GoToGround context) {
+            JSONObject decision = new JSONObject();
+            Commands command = new Commands();
+            Information info = context.getInfo();
+            JSONObject extras = info.getExtras();
+            Integer range;
+    
+            range = extras.getInt("range");
+            context.setFlyCount(range);
+            decision = command.fly();
+            context.setState(new FlyToGround());
+    
+            
+    
+            return decision;
+        }
+        
+        
+    }
+    private class TurnToGround implements GoToGroundState{
+
+        @Override
+        public JSONObject handleNextState(GoToGround context) {
+            JSONObject decision = new JSONObject();
+            Commands command = new Commands();
+            
+            decision = command.echo(context.getCurrentDirection());
+            context.setState(new GroundRange());
+    
+            return decision;
+        }
+        
+        
+    }
+    private class FlyToGround implements GoToGroundState{
+
+        @Override
+        public JSONObject handleNextState(GoToGround context) {
+            Commands command = new Commands();
+            JSONObject decision = new JSONObject();
+            
+            if(context.getFlyCount() > 0){
+                decision = command.fly();
+                context.setFlyCount(context.getFlyCount()- 1);
+                context.setState(new FlyToGround());
+            }else{
+                decision = command.scan();
+                context.setState(new Scan());
+    
+            }
+            
+    
+            
+    
+            return decision;
+        }
+    
+        
+    }
+    private class Scan implements GoToGroundState{
+    private final Logger logger = LogManager.getLogger();
+
+    @Override
+    public JSONObject handleNextState(GoToGround context) {
+        
+
+        JSONObject decision = new JSONObject();
+        Commands command = new Commands();
+        Information info = context.getInfo();
+        JSONObject extras = info.getExtras();
+
+        JSONArray biomesArray = extras.getJSONArray("biomes");
+
+            if (biomesArray.length() == 1 && ("OCEAN").equals(biomesArray.optString(0))){
+                logger.info("CHECKING BIOMES ARRAY");
+                decision = command.fly();
+                context.setState(new Fly());
+                
+            } else{
+                logger.info("NOT ON OCEAN");
+                context.setOnGround(true);
+                decision = command.fly();
+            }   
+        return decision;
+       
+    }
+    
+}
+
+    
+
+
+    
+    
+
+
 }
--- src/main/java/ca/mcmaster/se2aa4/island/team113/GoToGroundState.java
+++
@@ -1,7 +0,0 @@
-package ca.mcmaster.se2aa4.island.team113;
-
-import org.json.JSONObject;
-
-public interface GoToGroundState {
-    JSONObject handleNextState(GoToGround context);
-}
