9f6e67bdf32cf5423567fc6b5bb9f09c57a86e1d
Allen Tong <allentong@Allens-MacBook-Pro.local>
2024-03-21 15:35:41 -0400

    created tests for dronebattery class (close #44)

---
+++ src/test/java/ca/mcmaster/se2aa4/island/team113/DroneBatteryTest.java
@@ -0,0 +1,52 @@
+package ca.mcmaster.se2aa4.island.team113;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DroneBatteryTest {
+
+    private DroneBattery battery;
+
+    @BeforeEach
+    public void initializeBattery() {
+        battery = new DroneBattery(100);
+    }
+
+    @Test
+    public void shouldDecreaseBatteryBy10() {
+        battery.loseJuice(10);
+        assertEquals(90, battery.getCharge());
+    }
+
+    @Test
+    public void shouldDecreaseBatteryBy40() {
+        battery.loseJuice(40);
+        assertEquals(60, battery.getCharge());
+    }
+    
+    @Test
+    public void shouldBeLow() {
+        battery.loseJuice(80);
+        assertTrue(battery.lowcheck());
+    }
+
+    @Test
+    public void shouldNotBeLow() {
+        battery.loseJuice(70);
+        assertFalse(battery.lowcheck());
+    }
+
+    @Test
+    public void shouldBeLowEdgeCase() {
+        battery.loseJuice(74);
+        assertTrue(battery.lowcheck());
+    }
+
+    @Test
+    public void shouldNotBeLowEdgeCase() {
+        battery.loseJuice(73);
+        assertFalse(battery.lowcheck());
+    }
+
+}
